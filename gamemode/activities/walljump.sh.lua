673ea47aab87f1354e897236d1a55273a211a6d8
Pivius <pivisens@gmail.com>
2020-11-29 03:26:06 +0100

    Walljump activity

---
+++ gamemode/activities/walljump.sh.lua
@@ -0,0 +1,12 @@
+ActivityService.NewActivity{
+	name = "XWJ",
+	count = 0,
+	interval = 0,
+	backwards_count = 0
+}
+
+ActivityService.NewActivity{
+	name = "HWJ",
+	count_single = 0,
+	count_double = 0,
+}
