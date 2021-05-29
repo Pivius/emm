08636aa9eb7b9fc4b28b269f04d73863f1c8d0c5
Pivius <pivisens@gmail.com>
2021-05-29 22:07:45 +0200

    Taskservice remade

--- gamemode/util/task.sh.lua
+++ gamemode/util/task.sh.lua
@@ -1,72 +1,108 @@
-TaskService = TaskService or Class.New()
+TaskService = TaskService or {}
+TaskService.tasks = TaskService.tasks or {}
+
+
+-- # Properties
+
+function TaskService.InitPlayerProperties(ply)
+	ply.tasks = table.Copy(TaskService.tasks)
+end
+hook.Add(
+	SERVER and "InitPlayerProperties" or "InitLocalPlayerProperties",
+	"TaskService.InitPlayerProperties",
+	TaskService.InitPlayerProperties
+)
+
+function TaskService.Reload()
+	for _, ply in pairs(player.GetAll()) do
+		ply.tasks = table.Copy(TaskService.tasks)
+	end
+end
+hook.Add("OnReloaded", "TaskService.Reload", TaskService.Reload)
 
 
 -- # Utils
 
-function TaskService:Init()
-	self.players = {}
-end
+function TaskService.NewTask(task)
+	assert(task.key and task.name and task.hook, task.func, (task.key and "Missing task key") or (task.name and "Missing task key") or (task.hook and "Missing task hook") or (task.func and "Missing task function"))
 
-function TaskService:Hook(hook, identifier, func)
-	self.hook = hook
-	self.identifier = "TaskService." .. identifier
-	self.func = function(...)
+	local new_task = (task.inherit and table.Copy(TaskService.tasks[task.inherit])) or task
+	local key = task.key
+	local func = task.func
+
+	new_task.completed = false
+	new_task.running = false
+	new_task.func = function(...)
 		local var_args = {...}
 		local ply = var_args[1]
-
-		table.insert(var_args, 2, self)
-		table.insert(var_args, 3, self.players[ply])
+		table.insert(var_args, 2, ply.tasks[key])
 
 		if not IsEntity(ply) then
-			self:Stop()
+			TaskService.End(ply, key)
 		else
-			if not self:HasCompleted(ply) and self:IsRunningTask(ply) and func(unpack(var_args)) then
-				self:Complete(ply)
+			if not TaskService.HasCompleted(ply, key) and TaskService.IsRunningTask(ply, key) then
+				if func(unpack(var_args)) then
+					TaskService.Complete(ply, key)
+				end
 			end
 		end
 	end
-	
-	self:Start()
-end
 
-function TaskService:Start()
-	hook.Add(self.hook, self.identifier, self.func)
-end
-
-function TaskService:Stop()
-	hook.Remove(self.hook, self.identifier)
-end
-
-function TaskService:GetPlayerData(ply)
-	return self.players[ply]
-end
-
-function TaskService:AddPlayer(data)
-	data.completed = false
-	self.players[data.player] = data
-end
-
-function TaskService:RemovePlayer(ply)
-	self.players[ply] = false
-end
-
-function TaskService:Complete(ply)
-	self.players[ply].completed = true
-	hook.Call("Task_Complete", GAMEMODE, ply, self)
-end
-
-function TaskService:Clear()
-	self.players = {}
-end
-
-function TaskService:IsRunningTask(ply)
-	return not Falsy(self.players[ply])
-end
-
-function TaskService:HasCompleted(ply)
-	if self.players[ply] then
-		return self.players[ply].completed
+	if not TaskService.tasks[key] then
+		for _, ply in pairs(player.GetAll()) do
+			ply.tasks[key] = new_task
+		end
 	end
+
+	TaskService.tasks[key] = new_task
+	TaskService.Start(key)
+end
+
+function TaskService.SetPlayerTask(ply, task, tbl)
+	for k, v in pairs(tbl) do
+		if ply.tasks[task][k] then
+			if istable(ply.tasks[task][k]) then
+				table.insert(ply.tasks[task][k], v)
+			else
+				ply.tasks[task][k] = v
+			end
+		end
+	end
+end
+
+function TaskService.Complete(ply, task)
+	if ply.tasks[task].running then
+		ply.tasks[task].completed = true
+		hook.Call("TaskService.Complete", GAMEMODE, ply, ply.tasks[task])
+	end
+end
+
+function TaskService.Start(task)
+	local task = TaskService.tasks[task]
 	
-	return false
+	hook.Add(task.hook, "TaskService."..task.key, task.func)
+end
+
+function TaskService.Stop(task)
+	local task = TaskService.tasks[task]
+
+	hook.Remove(task.hook, "TaskService."..task.key)
+end
+
+function TaskService.IsRunningTask(ply, task)
+	return not Falsy(ply.tasks[task].running)
+end
+
+function TaskService.HasCompleted(ply, task)
+	return ply.tasks[task].completed
+end
+
+function TaskService.Reset(ply, task)
+	if istable(task) then
+		for k, v in pairs(task) do
+			ply.tasks[v] = table.Copy(TaskService.tasks[v])
+		end
+	else
+		ply.tasks[task] = TaskService.tasks[task]
+	end
 end
--- gamemode/util/tutorial.sh.lua
+++ gamemode/util/tutorial.sh.lua
@@ -18,33 +18,40 @@ hook.Add(
 function TutorialService.GetAngle(ply)
 	local pos = ply:GetPos()
 	local angle = ply:EyeAngles()
-	local distance = 50
-	local trace_east = util.TraceLine{
+	local fwd = angle:Forward():GetNormalized()
+	local right = Vector(fwd.y, -fwd.x, 0)
+	local distance = 75
+	
+	local trace_right = util.TraceLine{
 		start = pos,
-		endpos = pos + Vector(distance),
+		endpos = pos + right * distance,
 		mask = MASK_PLAYERSOLID_BRUSHONLY,
 		filter = ply
 	}
-	local trace_west = util.TraceLine{
+	local trace_left = util.TraceLine{
 		start = pos,
-		endpos = pos - Vector(distance),
-		mask = MASK_PLAYERSOLID_BRUSHONLY,
-		filter = ply
-	}
-	local trace_north = util.TraceLine{
-		start = pos,
-		endpos = pos + Vector(0, distance),
-		mask = MASK_PLAYERSOLID_BRUSHONLY,
-		filter = ply
-	}
-	local trace_south = util.TraceLine{
-		start = pos,
-		endpos = pos - Vector(0, distance),
+		endpos = pos - right * distance,
 		mask = MASK_PLAYERSOLID_BRUSHONLY,
 		filter = ply
 	}
 
-	return math.min(WalljumpService.GetAngle(angle:Right(), trace_east.HitNormal) + WalljumpService.GetAngle(angle:Right(), trace_west.HitNormal) + WalljumpService.GetAngle(angle:Right(), trace_north.HitNormal) + WalljumpService.GetAngle(angle:Right(), trace_south.HitNormal), 90)
+	return math.min(WalljumpService.GetAngle(angle:Right(), trace_right.HitNormal) + WalljumpService.GetAngle(angle:Right(), trace_left.HitNormal) , 90), trace_right.HitWorld, trace_left.HitWorld
+end
+
+function TutorialService.DetermineKeys(ply)
+	local angle, trace_right, trace_left = TutorialService.GetAngle(ply)
+
+	angle = math.Round(angle)
+
+	if not (trace_left or trace_right) or (45 > angle or angle > 58) then
+		return angle
+	elseif trace_left then
+		return "left"
+	elseif trace_right then
+		return "right"
+	end
+
+	return false
 end
 
 
@@ -53,7 +60,7 @@ end
 TUTORIAL_WALLJUMP = 1
 TUTORIAL_CORNERJUMP = 2
 TUTORIAL_WALLSLIDE = 3
-TUTORIAL_XWJ_ANGLE = 4
+TUTORIAL_ANGLE = 4
 TUTORIAL_VWJ = 5
 TUTORIAL_XWJ = 6
 
@@ -64,158 +71,144 @@ TutorialService.notification = {
 	[TUTORIAL_WALLJUMP] = "Stand next to a wall, hold spacebar and press any key pointing the opposite direction of the wall",
 	[TUTORIAL_CORNERJUMP] = "Hold W+A+D and press Spacebar while looking into a corner",
 	[TUTORIAL_WALLSLIDE] = "Right Click on your mouse to wall slide",
-	[TUTORIAL_XWJ_ANGLE] = ("Look at a wall at a 45-58 degree angle"),
+	[TUTORIAL_ANGLE] = ("Look at a wall at a 45-58 degree angle"),
 	[TUTORIAL_VWJ] = "Press and hold D+SPACE and rapidly press W+A.",
 	[TUTORIAL_VWJ + 0.5] = "Press and hold A+SPACE and rapidly press W+D.",
 	[TUTORIAL_XWJ] = "Press and hold W+D+SPACE and rapidly press A.",
 	[TUTORIAL_XWJ + 0.5] = "Press and hold W+A+SPACE and rapidly press D.",
 }
 
-TutorialService.tasks = TutorialService.tasks or {
-	TaskService.New(),
-	TaskService.New(),
-	TaskService.New(),
-	TaskService.New(),
-	TaskService.New(),
-	TaskService.New(),
-}
-
-TutorialService.tasks[TUTORIAL_WALLJUMP]:Hook("Walljump", "Tutorial.Walljump", function(ply, task)
-	if IsFirstTimePredicted() then
-		TutorialService.tasks[TUTORIAL_CORNERJUMP]:AddPlayer {
-			player = ply, 
-			count = 0, 
-			max_count = 10, 
-			type = "Cornerjump", 
-			tutorial = TutorialService.notification[TUTORIAL_CORNERJUMP]
-		}
-		ply.running_tutorial = TUTORIAL_CORNERJUMP
-		return true
-	end
-end)
-
-TutorialService.tasks[TUTORIAL_CORNERJUMP]:Hook("Activity.Cornerjump", "Tutorial.Cornerjump", function(ply, task, data)
-	if IsFirstTimePredicted() then
-		data.count = data.count + 1
-
-		if data.count >= data.max_count then
-			TutorialService.tasks[TUTORIAL_WALLSLIDE]:AddPlayer {
-				player = ply, 
-				type = "Wallslide",
-				tutorial = TutorialService.notification[TUTORIAL_WALLSLIDE]
-			}
-			ply.running_tutorial = TUTORIAL_WALLSLIDE
+TaskService.NewTask({
+	key = "tutorial_walljump",
+	name = "WalJump Tutorial",
+	description = TutorialService.notification[TUTORIAL_WALLJUMP],
+	hook = "Walljump",
+	func = function(ply, task)
+		if IsFirstTimePredicted() then
+			ply.running_tutorial = "tutorial_cornerjump"
+			ply.tasks["tutorial_cornerjump"].running = true
 			return true
 		end
 	end
-end)
+})
 
-TutorialService.tasks[TUTORIAL_WALLSLIDE]:Hook("Wallslide", "Tutorial.Wallslide", function(ply, task, data)
-	if IsFirstTimePredicted() then
-		TutorialService.tasks[TUTORIAL_XWJ_ANGLE]:AddPlayer {
-			player = ply, 
-			count =  0, 
-			max_count = 100, 
-			angles = 0,
-			type = "Finding the angle",
-			tutorial = TutorialService.notification[TUTORIAL_XWJ_ANGLE]
-		}
-		ply.running_tutorial = TUTORIAL_XWJ_ANGLE
-		return true
-	end
-end)
+TaskService.NewTask({
+	key = "tutorial_cornerjump",
+	name = "CornerJump Tutorial",
+	description = TutorialService.notification[TUTORIAL_CORNERJUMP],
+	hook = "Activity.Cornerjump",
+	count = 0,
+	max_count = 10,
+	func = function(ply, task)
+		if IsFirstTimePredicted() then
+			task.count = task.count + 1
 
-TutorialService.tasks[TUTORIAL_XWJ_ANGLE]:Hook("Move", "Tutorial.XWJAngle", function(ply, task, data)
-	if IsFirstTimePredicted() then
-		data.angle = math.Round(TutorialService.GetAngle(ply))
-
-		if ply.walljump_max_angle > data.angle and data.angle > 45 then
-			data.count = data.count + 1
-
-			if data.count >= data.max_count then
-				local right = ply:EyeAngles():Forward():GetNormalized()
-				local trace_left = WalljumpService.Trace(ply, -Vector(right.y, -right.x, 0))
-
-				TutorialService.tasks[TUTORIAL_VWJ]:AddPlayer {
-					player = ply, 
-					count = 0, 
-					max_count = 10, 
-					type = "Vertical Walljump",
-					tutorial = (trace_left.HitWorld and TutorialService.notification[TUTORIAL_VWJ]) or TutorialService.notification[TUTORIAL_VWJ + 0.5]
-				}
-				ply.running_tutorial = TUTORIAL_VWJ
+			if task.count >= task.max_count then
+				ply.running_tutorial = "tutorial_wallslide"
+				ply.tasks["tutorial_wallslide"].running = true
 				return true
 			end
 		end
 	end
-end)
+})
 
-TutorialService.tasks[TUTORIAL_VWJ]:Hook("Activity.VWJ", "Tutorial.VWJ", function(ply, task, data)
-	if IsFirstTimePredicted() then
-		data.count = data.count + 1
+TaskService.NewTask({
+	key = "tutorial_wallslide",
+	name = "Wallslide Tutorial",
+	description = TutorialService.notification[TUTORIAL_WALLSLIDE],
+	hook = "Wallslide",
+	time = 0,
+	max_time = 3,
+	func = function(ply, task)
+		if IsFirstTimePredicted() then
+			task.time = task.time + FrameTime()
 
-		if data.count >= data.max_count then
-			local right = ply:EyeAngles():Forward():GetNormalized()
-			local trace_left = WalljumpService.Trace(ply, -Vector(right.y, -right.x, 0))
-
-			TutorialService.tasks[TUTORIAL_XWJ]:AddPlayer {
-				player = ply, 
-				count =  0, 
-				max_count = 10, 
-				type = "Extreme Walljump",
-				tutorial = (trace_left.HitWorld and TutorialService.notification[TUTORIAL_XWJ]) or TutorialService.notification[TUTORIAL_XWJ + 0.5]
-			}
-			ply.running_tutorial = TUTORIAL_XWJ
-			return true
+			if task.time >= task.max_time then
+				ply.running_tutorial = "tutorial_vwj"
+				ply.tasks["tutorial_vwj"].running = true
+				return true
+			end
 		end
 	end
-end)
+})
 
-TutorialService.tasks[TUTORIAL_XWJ]:Hook("Activity.XWJ", "Tutorial.XWJ", function(ply, task, data)
-	if IsFirstTimePredicted() then
-		data.count = data.count + 1
+TaskService.NewTask({
+	key = "tutorial_vwj",
+	name = "VWJ Tutorial",
+	description = TutorialService.notification[TUTORIAL_ANGLE],
+	hook = "Move", --"Activity.VWJ",
+	count = 0,
+	max_count = 10,
+	angle = 0,
+	func = function(ply, task)
+		local trace = TutorialService.DetermineKeys(ply)
 
-		if data.count >= data.max_count then
-			ply.running_tutorial = false
-			return true
+		if isnumber(trace) then
+			task.description = TutorialService.notification[TUTORIAL_ANGLE]
+			task.angle = trace
+		elseif trace == "left" then
+			task.description = TutorialService.notification[TUTORIAL_VWJ]
+			task.angle = nil
+		elseif trace == "right" then
+			task.description = TutorialService.notification[TUTORIAL_VWJ + 0.5]
+			task.angle = nil
+		end
+
+		if IsFirstTimePredicted() and ActivityService.IsTriggered(ply, "vwalljump") then
+			task.count = task.count + 1
+
+			if task.count >= task.max_count then
+				ply.running_tutorial = "tutorial_xwj"
+				ply.tasks["tutorial_xwj"].running = true
+				return true
+			end
 		end
 	end
-end)
+})
 
+TaskService.NewTask({
+	key = "tutorial_xwj",
+	name = "XWJ Tutorial",
+	description = TutorialService.notification[TUTORIAL_ANGLE],
+	hook = "Move", --"Activity.VWJ",
+	count = 0,
+	max_count = 10,
+	angle = 0,
+	func = function(ply, task)
+		local trace = TutorialService.DetermineKeys(ply)
 
-function TutorialService.DetermineKeys(ply, move)
-	if ply.running_tutorial == TUTORIAL_VWJ or ply.running_tutorial == TUTORIAL_XWJ then
-		local angle = math.Round(TutorialService.GetAngle(ply))
-		local right = ply:EyeAngles():Forward():GetNormalized()
-		local trace_left, trace_right
+		if isnumber(trace) then
+			task.description = TutorialService.notification[TUTORIAL_ANGLE]
+			task.angle = trace
+		elseif trace == "left" then
+			task.description = TutorialService.notification[TUTORIAL_XWJ]
+			task.angle = nil
+		elseif trace == "right" then
+			task.description = TutorialService.notification[TUTORIAL_XWJ + 0.5]
+			task.angle = nil
+		end
 
-		right = Vector(right.y, -right.x, 0)
-		trace_left = WalljumpService.Trace(ply, -right)
-		trace_right = WalljumpService.Trace(ply, right)
+		if IsFirstTimePredicted() and ActivityService.IsTriggered(ply, "xwalljump") then
+			task.count = task.count + 1
 
-		if not (trace_left.HitWorld or trace_right.HitWorld) or (45 > angle or angle > 58) then
-			TutorialService.tasks[ply.running_tutorial]:GetPlayerData(ply).tutorial = TutorialService.notification[TUTORIAL_XWJ_ANGLE]
-			TutorialService.tasks[ply.running_tutorial]:GetPlayerData(ply).angle = angle
-		else
-			TutorialService.tasks[ply.running_tutorial]:GetPlayerData(ply).tutorial = (trace_left.HitWorld and TutorialService.notification[ply.running_tutorial]) or TutorialService.notification[ply.running_tutorial + 0.5]
-			TutorialService.tasks[ply.running_tutorial]:GetPlayerData(ply).angle = nil
+			if task.count >= task.max_count then
+				ply.running_tutorial = false
+				return true
+			end
 		end
 	end
-end
-hook.Add("Move", "TutorialService.DetermineKeys", TutorialService.DetermineKeys)
+})
 
 
 -- # Command
 
 function TutorialService.Command(ply)
 	if not ply.running_tutorial then
-		ply.running_tutorial = TUTORIAL_WALLJUMP
-		TutorialService.tasks[TUTORIAL_WALLJUMP]:AddPlayer {
-			player = ply, 
-			type = "Walljump", 
-			tutorial = TutorialService.notification[TUTORIAL_WALLJUMP]
-		}
+		ply.running_tutorial = "tutorial_vwj"
+		TaskService.Reset(ply, {"tutorial_walljump", "tutorial_cornerjump", "tutorial_wallslide", "tutorial_vwj", "tutorial_xwj"})
+		ply.tasks["tutorial_vwj"].running = true
 	else
+		TaskService.Reset(ply, {"tutorial_walljump", "tutorial_cornerjump", "tutorial_wallslide", "tutorial_vwj", "tutorial_xwj"})
 		ply.running_tutorial = false
 	end
 end
